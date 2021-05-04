d0eed4b5489bcd5c93b321697644b253d639a3f3
diane <dianine23@gmail.com>
2021-05-04 20:28:48 -0300

    _5Projeto

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		double pi = 3.14159;
+		double R = sc.nextDouble();
+		
+		
+		
+		double A = pi*R*R;
+		
+		System.out.printf("Area: %.4f%n", A);
+		
+		sc.close();
+		
+	}
+
+}
